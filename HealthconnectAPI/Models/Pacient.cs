42ecfb1fddc0e901c94f6708de1949d03b4d5d1e
Vlad <vlad.orbulescu@bluecouchdigital.com>
2017-05-03 22:09:36 +0300

    User fetching from database with encryption

---
+++ HealthconnectAPI/Models/Pacient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthconnectAPI.Models
+{
+    public class Pacient
+    {
+        public int Id { get; set; }
+        public string Cnp { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
