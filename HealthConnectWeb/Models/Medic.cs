268dcf54eef5edfa71d5911c03e8277051b81b31
Vlad <vlad.orbulescu@bluecouchdigital.com>
2017-05-03 22:04:51 +0300

    User login session implemented with cookies

---
+++ HealthConnectWeb/Models/Medic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthConnectWeb.Models
+{
+    public class Medic
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+    }
+}
